e0721ec891aa9edc67d26072c64b7a1c84cf45c3
imryziv <imryziv@mail.tau.ac.il>
2025-01-22 12:28:34 +0200

    update conda setup with req.txt download.

--- conda-setup.sh
+++ conda-setup.sh
@@ -1,16 +1,24 @@
-# create your workshop directory
+# Create your workshop directory
 WORKDIR=/home/tkoren/mldaw2425/imryziv
-mkdir $WORKDIR
+mkdir -p $WORKDIR
 cd $WORKDIR
 
-# download and install Miniconda
-wget <
-https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh>
+# Download and install Miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash $WORKDIR/Miniconda3-latest-Linux-x86_64.sh -b -p $WORKDIR/miniconda3
 
-# comment the following lines if you do not want to install the packages
-source ~/.bashrc # this is necessary in order to use the newly installed conda command
+# Comment the following lines if you do not want to install the packages
+source ~/.bashrc # This is necessary to use the newly installed conda command
 conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
 pip install notebook
 
-rm Miniconda3-latest-Linux-x86_64.sh
+# Download requirements.txt for Detect_AI_Generated_Text
+PROJECT_DIR=$WORKDIR/Detect_AI_Generated_Text
+mkdir -p $PROJECT_DIR
+wget -O $PROJECT_DIR/requirements.txt https://example.com/path/to/Detect_AI_Generated_Text/requirements.txt
+
+# Install dependencies from requirements.txt
+pip install -r $PROJECT_DIR/requirements.txt
+
+# Clean up the Miniconda installer
+rm $WORKDIR/Miniconda3-latest-Linux-x86_64.sh
